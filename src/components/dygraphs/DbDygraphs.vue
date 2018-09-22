c70927f9d23f27e454387c21970e031a04e7c7c3
sv2 <sv2@slana.tech>
2018-09-22 11:17:07 -0700

    initial

---
+++ src/components/dygraphs/DbDygraphs.vue
@@ -0,0 +1,41 @@
+<template>
+  <div ref="container" class="dbc-dygraphs"/>
+</template>
+<script>
+import Dygraphs from "dygraphs";
+
+export default {
+  props: {
+    data: {},
+    options: {}
+  },
+  data() {
+    return {
+      graph: null
+    };
+  },
+  mounted() {
+    this.render();
+  },
+  watch: {
+    data: function() {
+      // Rendering is triggered when data changed
+      // To force re-render if only options changed, call $refs.child.render
+      this.render();
+    }
+  },
+  methods: {
+    render() {
+      //console.log("Rendering Dtgraph ...");
+      this.graph = new Dygraphs(this.$refs.container, this.data, this.options);
+    }
+  }
+};
+</script>
+<style>
+.dbc-dygraphs {
+  width: 100%;
+  height: 95%;
+  display: block;
+}
+</style>
---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+import HelloWorld from "./HelloWorld.vue";
+import DbDygraphs from "./dygraphs/DbDygraphs.vue";
+
+const Components = {
+  HelloWorld,
+  DbDygraphs
+};
+
+export default Components;
