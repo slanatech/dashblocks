4230ad530ad1fb4ab4c60d042bd34a92379e1b8d
sv2 <sv2@slana.tech>
2019-10-05 11:11:13 -0700

    Spark line and area

--- src/components/db/DbSparkline.vue
+++ src/components/db/DbSparkline.vue
@@ -5,7 +5,7 @@
   </div>
 </template>
 <script>
-import { curve } from '../utils/cardinal-spline-js/curve_func';
+// import { curve } from '../utils/cardinal-spline-js/curve_func';
 import * as d3 from 'd3';
 export default {
   name: 'DbSparkline',
@@ -49,6 +49,8 @@ export default {
         this.render();
       });
     },
+    //https://github.com/d3/d3-shape#curves
+    //https://bl.ocks.org/d3indepth/64be9fc39a92ef074034e9a8fb29dcce
     render() {
       this.initSize();
       let canvas = this.$refs.canvas;
@@ -56,7 +58,6 @@ export default {
       canvas.height = this.lineHeight; // What is this ? Padding + border ?
       let ctx = canvas.getContext('2d');
       ctx.clearRect(0, 0, canvas.width, canvas.height);
-      ctx.beginPath();
 
       // TODO Erase everything
       if (!this.data || this.data.length <= 0) {
@@ -64,34 +65,58 @@ export default {
       }
 
       // Scale
-      let x = d3
+      let lx = d3
         .scaleLinear()
         .domain([0, this.data.length])
         .range([0, canvas.width]);
 
-      let y = d3
+      let ly = d3
         .scaleLinear()
         .domain([0, d3.max(this.data)])
-        .range([0, canvas.height]);
+        .range([canvas.height, 0]);
 
+      let pointsData = this.data.map((d, i) => {
+        return { x: i, y: d };
+      });
+
+      let line = d3
+        .line()
+        .x(d => lx(d.x))
+        .y(d => ly(d.y))
+        .curve(d3.curveCardinal);
+      line.context(ctx);
+
+      let area = d3
+        .area()
+        .x(d => lx(d.x))
+        .y1(d => ly(d.y))
+        .y0(canvas.height)
+        .curve(d3.curveCardinal);
+      area.context(ctx);
+
+      ctx.beginPath();
+      //line(pointsData);
+      area(pointsData);
+      ctx.fillStyle = 'rgba(0, 255, 0, 0.2)';
+      ctx.fill();
+      //ctx.stroke();
+
+      //let lineGenerator = d3.line().curve(d3.curveCardinal);
+      //let pathData = lineGenerator(curveData);
+      /*
       let ymax = canvas.height;
       let curveData = [];
       for (let i = 0; i < this.data.length; i++) {
         curveData.push(x(i));
         curveData.push(ymax - y(this.data[i]));
       }
-
+      */
       // !!! See !!!
-      //https://github.com/d3/d3-shape#curves
-      //https://bl.ocks.org/d3indepth/64be9fc39a92ef074034e9a8fb29dcce
-
-      let curvePoints = curve(ctx, curveData);
-
-      ctx.lineTo(curvePoints[curvePoints.length - 2], ymax); // bottom-right
-      ctx.lineTo(curvePoints[0], ymax); // bottom-left
-
-      ctx.fillStyle = 'rgba(0, 255, 0, 0.2)';
-      ctx.fill();
+      //let curvePoints = [0,0]; //curve(ctx, curveData);
+      //ctx.lineTo(curvePoints[curvePoints.length - 2], ymax); // bottom-right
+      //ctx.lineTo(curvePoints[0], ymax); // bottom-left
+      //ctx.fillStyle = 'rgba(0, 255, 0, 0.2)';
+      //ctx.fill();
       //ctx.stroke();
     }
   }
