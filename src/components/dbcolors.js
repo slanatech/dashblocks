cf61f959b6cbeafd23e3ad347f441747b91fb0c5
sv2 <sv2@slana.tech>
2019-11-21 23:59:14 -0800

    Colors

--- src/components/dbcolors.js
+++ src/components/dbcolors.js
@@ -4,6 +4,7 @@
 import * as d3ScaleChromatic from 'd3-scale-chromatic';
 import * as d3Color from 'd3-color';
 //import log from './log';
+//import gColors from './utils/colors';
 
 // TODO More D3 colors
 // TODO Color definitions based on base set - normal, warning, alarm ...
@@ -16,6 +17,7 @@ import * as d3Color from 'd3-color';
 
 //const d3Colors = [ '#1f77b4','#ff7f0e','#2ca02c','#d62728','#9467bd','#8c564b','#e377c2','#7f7f7f','#bcbd22','#17becf'];
 
+// eslint-disable-next-line
 const grafanaColors = [
   '#7EB26D', // 0: pale green
   '#EAB839', // 1: mustard
@@ -75,15 +77,54 @@ const grafanaColors = [
   '#DEDAF7'
 ];
 
+// eslint-disable-next-line
+const carbonDarkR = [
+  '#007d79',
+  '#ff7eb6',
+  '#fa4d56',
+  '#fff1f1',
+  '#6fdc8c',
+  '#4589ff',
+  '#d12771',
+  '#d2a106',
+  '#08bdba',
+  '#bae6ff',
+  '#ba4e00',
+  '#d4bbff',
+  '#8a3ffc',
+  '#33b1ff'
+];
+
+// eslint-disable-next-line
+const diverging1 = ['#a50026', '#d73027', '#f46d43', '#fdae61', '#fee090', '#ffffbf', '#e0f3f8', '#abd9e9', '#74add1', '#4575b4', '#313695'];
+// eslint-disable-next-line
+const diverging2 = ['#8e0152', '#c51b7d', '#de77ae', '#f1b6da', '#fde0ef', '#f7f7f7', '#e6f5d0', '#b8e186', '#7fbc41', '#4d9221', '#276419'];
+// eslint-disable-next-line
+const diverging3 = ['#a50026', '#d73027', '#f46d43', '#fdae61', '#fee08b', '#ffffbf', '#d9ef8b', '#a6d96a', '#66bd63', '#1a9850', '#006837'];
+
 /* DashBlocks Colors */
 class DbColors {
   constructor() {
     // TODO //
+    this.spectralDiverging = d3ScaleChromatic.schemeSpectral[4];
+    this.a = d3ScaleChromatic.schemeRdYlBu[6];
+    this.b = this.a.reverse();
   }
+
+  // TODO getColors by "palette"
+  // App may define multiple palettes.
+  // "default" is always defined, and used if no palette name is passed to the chart
+  // colorPalette: <name> is standard prop for chart components
+  // palette defines both light and dark sets
+  // plus more, like alarm / normal / warning colors
 
   getColors(dark) {
     //return dark ? d3ScaleChromatic.schemePastel1 : d3ScaleChromatic.schemeSet1;
-    return dark ? grafanaColors : d3ScaleChromatic.schemeCategory10;
+    //return dark ? grafanaColors : d3ScaleChromatic.schemeCategory10;
+    //return dark ? d3ScaleChromatic.schemeTableau10 : d3ScaleChromatic.schemeCategory10;
+    //let a = gColors;
+    //return dark ? d3ScaleChromatic.schemeSet3 : d3ScaleChromatic.schemeCategory10;
+    return dark ? this.b : d3ScaleChromatic.schemeTableau10;
   }
 
   hex2RGBA(hex, opacity) {
