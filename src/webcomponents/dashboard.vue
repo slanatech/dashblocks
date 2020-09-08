14b957e8c0818b95786406cc1d58613ca43ec071
sv2 <sv2@slana.tech>
2020-09-08 16:34:50 -0700

    Web Components

--- src/webcomponents/dashboard.vue
+++ src/webcomponents/dashboard.vue
@@ -1,20 +1,36 @@
 <script>
 import Vue from 'vue';
 
-// For testing - Import and register all individual components
-//import DbDashboard from '../components/dashboard/DbDashboard?shadow';
-//import DbDygraphs from '../components/dygraphs/DbDygraphs';
-//Vue.component('DbDygraphs', DbDygraphs);
-//console.log(`Registered DbDygraphs`);
+import DbDashboard from '../components/dashboard/DbDashboard';
 
-// For full build
-import * as DbComponents from '../components.js';
-for (let m of Object.keys(DbComponents)) {
-  if (m !== 'DbDashboard') {
-    Vue.component(m, DbComponents[m]);
-    console.log(`Registered ${m}`);
-  }
-}
+import { DbDygraphs, DbDygraphsBar, DbDygraphsLine, DbDygraphsSparkLine } from '../components/dygraphs/index.js';
+Vue.component('DbDygraphs', DbDygraphs);
+Vue.component('DbDygraphsBar', DbDygraphsBar);
+Vue.component('DbDygraphsLine', DbDygraphsLine);
+Vue.component('DbDygraphsSparkLine', DbDygraphsSparkLine);
+
+import {
+  DbChartjsBar,
+  DbChartjsHorizontalBar,
+  DbChartjsDoughnut,
+  DbChartjsLine,
+  DbChartjsPie,
+  DbChartjsPolarArea,
+  DbChartjsRadar,
+  DbChartjsBubble,
+  DbChartjsScatter,
+  DbChartjsFunnel
+} from '../components/chartjs/index.js';
+Vue.component('DbChartjsBar', DbChartjsBar);
+Vue.component('DbChartjsHorizontalBar', DbChartjsHorizontalBar);
+Vue.component('DbChartjsDoughnut', DbChartjsDoughnut);
+Vue.component('DbChartjsLine', DbChartjsLine);
+Vue.component('DbChartjsPie', DbChartjsPie);
+Vue.component('DbChartjsPolarArea', DbChartjsPolarArea);
+Vue.component('DbChartjsRadar', DbChartjsRadar);
+Vue.component('DbChartjsBubble', DbChartjsBubble);
+Vue.component('DbChartjsScatter', DbChartjsScatter);
+Vue.component('DbChartjsFunnel', DbChartjsFunnel);
 
 export default DbDashboard;
 </script>
